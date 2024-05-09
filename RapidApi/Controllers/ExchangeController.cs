3ece9dc552569cfff00e6e91ddef1b603ef2d870
Yasin Baltaoğlu <37988334+ayasinb@users.noreply.github.com>
2024-05-09 21:49:58 +0300

    IMDB Series top100 movie.

--- RapidApi/Controllers/ExchangeController.cs
+++ RapidApi/Controllers/ExchangeController.cs
@@ -1,29 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using RapidApi.Models;
 
 namespace RapidApi.Controllers
 {
-    public class ImdbSeriesController : Controller
+    public class ExchangeController : Controller
     {
         public async Task<IActionResult> Index()
         {
+           
             var client = new HttpClient();
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/currency-exchange-rate?from_symbol=USD&language=en&to_symbol=TRY"),
                 Headers =
     {
         { "x-rapidapi-key", "4b38bf5c65msh4807436cd4553b5p129eefjsneb11faede721" },
-        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+        { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
     },
             };
             using (var response = await client.SendAsync(request))
             {
                 response.EnsureSuccessStatusCode();
                 var body = await response.Content.ReadAsStringAsync();
-                var values=JsonConvert.DeserializeObject<List<ImdbSeriesViewModel>>(body);
+                var values=JsonConvert.DeserializeObject<>(body);
                 return View(values);
             }
             
