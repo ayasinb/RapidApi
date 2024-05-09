bcbe4fbfb70a8ddb954fe04c6de2693a6ac1132e
Yasin Baltaoğlu <37988334+ayasinb@users.noreply.github.com>
2024-05-09 21:28:50 +0300

    Add project files.

---
+++ RapidApi/Controllers/ImdbSeriesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RapidApi.Controllers
+{
+    public class ImdbSeriesController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "x-rapidapi-key", "4b38bf5c65msh4807436cd4553b5p129eefjsneb11faede721" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+            }
+            return View();
+        }
+    }
+}
